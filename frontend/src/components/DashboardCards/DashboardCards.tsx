9679fe1e48b958a80f801455b2bd29298c85dfff
Hektor-Augusto <hektoraugusto.s@gmail.com>
2025-04-21 11:39:13 -0300

    fix(mobile status in cards)

--- frontend/src/components/DashboardCards/DashboardCards.tsx
+++ frontend/src/components/DashboardCards/DashboardCards.tsx
@@ -16,8 +16,8 @@ export default function DashboardCards({
   totalStockValue,
 }: DashboardCardsProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-6">
-      <div className="bg-white rounded-lg shadow p-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 w-full">
+      <div className="bg-white rounded-lg shadow p-4 w-full sm:max-w-xs max-w-[15rem]">
         <h2 className="text-black-600 dark:text-black-300 text-sm text-center">
           Produtos Vendidos
         </h2>
@@ -25,15 +25,12 @@ export default function DashboardCards({
           <Image src={Money} alt="Ilustração de dinheiro" />
           <p className="text-2xl font-bold text-blue-500">
             +{" "}
-            {totalSold.toLocaleString("pt-BR", {
-              minimumFractionDigits: 0,
-              maximumFractionDigits: 0,
-            })}
+            {totalSold.toLocaleString("pt-BR")}
           </p>
         </div>
       </div>
 
-      <div className="bg-white rounded-lg shadow p-4">
+      <div className="bg-white rounded-lg shadow p-4 w-full sm:max-w-xs max-w-[15rem]">
         <h2 className="text-black-600 dark:text-black-300 text-sm text-center">
           Em Estoque (Qtde)
         </h2>
@@ -41,23 +38,19 @@ export default function DashboardCards({
           <Image src={Money} alt="Ilustração de dinheiro" />
           <p className="text-2xl font-bold text-green-500">
             +{" "}
-            {totalStock.toLocaleString("pt-BR", {
-              minimumFractionDigits: 0,
-              maximumFractionDigits: 0,
-            })}
+            {totalStock.toLocaleString("pt-BR")}
           </p>
         </div>
       </div>
 
-      <div className="bg-white rounded-lg shadow p-4">
+      <div className="bg-white rounded-lg shadow p-4 w-full sm:max-w-xs max-w-[15rem]">
         <h2 className="text-black-600 dark:text-black-300 text-sm text-center">
           Valor Total do Estoque
         </h2>
         <div className="flex gap-4 justify-center items-center">
           <Image src={Money} alt="Ilustração de dinheiro" />
           <p className="text-2xl font-bold text-yellow-500">
-            R${" "}
-            {totalStockValue.toLocaleString("pt-BR", {
+            R$ {totalStockValue.toLocaleString("pt-BR", {
               minimumFractionDigits: 2,
               maximumFractionDigits: 2,
             })}
